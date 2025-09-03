e7ac6ef0657100afe8e59f3c28ba76f89cd1d95f
Raí <raicarvalho343@gmail.com>
2025-09-03 16:11:45 -0300

    Criado form manual para contatos

--- heroes/urls.py
+++ heroes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hello/', views.hello_heroes, name='hello_heroes'),
     path('lista/', views.lista_herois, name='lista_herois'),
     path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
+    path('contato/', views.contato_view, name='contato'),
 ]
