7c67e25a3509f0766c6e91fff0dc7408f2324c63
Raí <raicarvalho343@gmail.com>
2025-10-09 16:37:43 -0300

    não sei o que tem, mas não quero perder

--- villains/urls.py
+++ villains/urls.py
@@ -1,8 +1,11 @@
-# villains/urls.py
 from django.urls import path
-from .views import lista_viloes
+from . import views
 
 urlpatterns = [
-    path('', lista_viloes, name='lista_viloes'),
+    path("", views.VillainListView.as_view(), name="lista_viloes"),
+    path("novo/", views.VillainCreateView.as_view(), name="novo_vilao"),
+    path("<int:pk>/editar/", views.VillainUpdateView.as_view(), name="editar_vilao"),
+    path("<int:pk>/excluir/", views.VillainDeleteView.as_view(), name="excluir_vilao"),
+    path("<int:pk>/", views.VillainDetailView.as_view(), name="detalhe_vilao"),
 ]
 
